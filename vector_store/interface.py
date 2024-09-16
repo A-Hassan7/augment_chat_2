7e933c642ba64525b61f4f97a8aeaefa9083958b
A-hassan7 <alshassan@hotmail.com>
2024-09-16 20:53:19 +0100

    feat: run worker for vectorstore

--- vector_store/interface.py
+++ vector_store/interface.py
@@ -7,4 +7,7 @@ class VectorStoreInterface:
         self.vector_store_queue = VectorStoreQueue()
 
     def enqueue_message(self, parsed_message):
-        self.vector_store_queue.enqueue_message(parsed_message)
+        return self.vector_store_queue.enqueue_message(parsed_message)
+
+    def run_worker(self):
+        self.vector_store_queue.run_worker()
