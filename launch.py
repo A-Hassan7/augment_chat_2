4400a3043085ff869a42ac6a4bc0b26145433809
A-hassan7 <alshassan@hotmail.com>
2024-10-13 17:15:19 +0100

    feat: main launch script implementing both debugging mode and asynchronous workers mode.
    Debug mode - runs all task queues synchornously so every task is executed when an event happens through the event listener
    
    Asynchronous workers mode - creates subprocesses for each queue worker and the event listener which all run in the background.

--- launch.py
+++ launch.py
@@ -1,17 +1,39 @@
-from event_processor import EventProcessorInterface
 from config import GlobalConfig
 
 # when running in debug mode I only need to initialise the event listener
 # the event listener acts as the triggering event all/most other processes
 # setting GlobalConfig.DEBUG_MODE = True puts the event queue in synchronouse mode
 # which tells it to execute processes on the same process/thread
-# debug mode also uses a fake redis emulator so a real redis instance is not required.
-debug = True
+debug = False
+
 if debug:
     GlobalConfig.DEBUG_MODE = True
+    GlobalConfig.USE_FAKE_REDIS = False
 
-    interface = EventProcessorInterface()
-    interface.run_event_listener()
+    from event_processor import EventProcessorInterface
+
+    EventProcessorInterface().run_event_listener()
+
+    # suggestions = Suggestions()
+    # suggestions.generate_jokes(
+    #     "!cmUhTOOmdBzQRDPHUw:matrix.localhost.me",
+    #     until_message_event_id="$yFTdOimYAahaU_CqOKstKbQyVza8Z3QV24CgtG-gRBo",
+    # )
+
+import asyncio
+
+commands = [
+    "python -c 'from event_processor import EventProcessorInterface; EventProcessorInterface().run_event_listener();'",
+    "python -c 'from event_processor import EventProcessorInterface; EventProcessorInterface().run_event_processor_worker();'",
+    "python -c 'from vector_store import VectorStoreInterface; VectorStoreInterface().run_worker();'",
+    "python -c 'from llm_service import LLMInterface; LLMInterface().run_worker();'",
+]
 
 
-# in non debug mode I need to create workers so I'll have to initialise services individually
+# create a subprocess for each queue worker
+async def main():
+    processes = [await asyncio.create_subprocess_shell(cmd) for cmd in commands]
+    outputs = [await process.wait() for process in processes]
+
+
+asyncio.run(main())
